e53ffadf326a69cd116ad2bcccfb2cd9cf9b10e3
queoisme <thiennguyen.csnt@gmail.com>
2025-08-09 15:43:38 +0700

    stl

---
+++ STL/Vector.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define el cout << '\n'
+#define L(i, a, b) for (int i = (a); i <= (b); i++)
+#define R(i, a, b) for (int i = (a); i >= (b); i--)
+#define gg(...) [](const auto&...x){ char c='='; cerr<<#__VA_ARGS__<<" ";((cerr<<exchange(c,',')<<" "<<x),...); cerr<<endl; }(__VA_ARGS__);
+#define N (int)
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
+    cout << 3; 
+}
